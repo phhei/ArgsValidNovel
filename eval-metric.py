688594d16f950216fd7776eece1349377b6bcfab
Philipp Heinisch <pheinisch@techfak.uni-bielefeld.de>
2022-08-01 09:17:21 +0200

    Added f1_val_macro/ f1_nov_macro

--- eval-metric.py
+++ eval-metric.py
@@ -12,8 +12,14 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
         "true_positive_validity": numpy.sum(numpy.where(
             numpy.all(numpy.stack([is_validity >= .5, should_validity >= .5]), axis=0),
             1, 0)),
+        "true_negative_validity": numpy.sum(numpy.where(
+            numpy.all(numpy.stack([is_validity < .5, should_validity < .5]), axis=0),
+            1, 0)),
         "true_positive_novelty": numpy.sum(numpy.where(
             numpy.all(numpy.stack([is_novelty >= .5, should_novelty >= .5]), axis=0),
+            1, 0)),
+        "true_negative_novelty": numpy.sum(numpy.where(
+            numpy.all(numpy.stack([is_novelty < .5, should_novelty < .5]), axis=0),
             1, 0)),
         "true_positive_valid_novel": numpy.sum(numpy.where(
             numpy.all(numpy.stack([is_validity >= .5, is_novelty >= .5,
@@ -32,7 +38,9 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
                                    should_validity < .5, should_novelty < .5]), axis=0),
             1, 0)),
         "classified_positive_validity": numpy.sum(numpy.where(is_validity >= .5, 1, 0)),
+        "classified_negative_validity": numpy.sum(numpy.where(is_validity < .5, 1, 0)),
         "classified_positive_novelty": numpy.sum(numpy.where(is_novelty >= .5, 1, 0)),
+        "classified_negative_novelty": numpy.sum(numpy.where(is_novelty < .5, 1, 0)),
         "classified_positive_valid_novel": numpy.sum(numpy.where(
             numpy.all(numpy.stack([is_validity >= .5, is_novelty >= .5]), axis=0),
             1, 0)),
@@ -46,7 +54,9 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
             numpy.all(numpy.stack([is_validity < .5, is_novelty < .5]), axis=0),
             1, 0)),
         "indeed_positive_validity": numpy.sum(numpy.where(should_validity >= .5, 1, 0)),
+        "indeed_negative_validity": numpy.sum(numpy.where(should_validity < .5, 1, 0)),
         "indeed_positive_novelty": numpy.sum(numpy.where(should_novelty >= .5, 1, 0)),
+        "indeed_negative_novelty": numpy.sum(numpy.where(should_novelty < .5, 1, 0)),
         "indeed_positive_valid_novel": numpy.sum(numpy.where(
             numpy.all(numpy.stack([should_validity >= .5, should_novelty >= .5]), axis=0),
             1, 0)),
@@ -70,6 +80,14 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
                            max(1, ret_base_help["indeed_positive_validity"]),
         "recall_novelty": ret_base_help["true_positive_novelty"] /
                           max(1, ret_base_help["indeed_positive_novelty"]),
+        "precision_val_neg": ret_base_help["true_negative_validity"] /
+                              max(1, ret_base_help["classified_negative_validity"]),
+        "precision_nov_neg": ret_base_help["true_negative_novelty"] /
+                             max(1, ret_base_help["classified_negative_novelty"]),
+        "recall_val_neg": ret_base_help["true_negative_validity"] /
+                           max(1, ret_base_help["indeed_negative_validity"]),
+        "recall_nov_neg": ret_base_help["true_negative_novelty"] /
+                          max(1, ret_base_help["indeed_negative_novelty"]),
         "precision_valid_novel": ret_base_help["true_positive_valid_novel"] /
                                  max(1, ret_base_help["classified_positive_valid_novel"]),
         "precision_valid_nonnovel": ret_base_help["true_positive_valid_nonnovel"] /
@@ -91,6 +109,8 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
     ret.update({
         "f1_validity": 2 * ret_help["precision_validity"] * ret_help["recall_validity"] / max(1e-4, ret_help["precision_validity"] + ret_help["recall_validity"]),
         "f1_novelty": 2 * ret_help["precision_novelty"] * ret_help["recall_novelty"] / max(1e-4, ret_help["precision_novelty"] + ret_help["recall_novelty"]),
+        "f1_val_neg": 2 * ret_help["precision_val_neg"] * ret_help["recall_val_neg"] / max(1e-4, ret_help["precision_val_neg"] + ret_help["recall_val_neg"]),
+        "f1_nov_neg": 2 * ret_help["precision_nov_neg"] * ret_help["recall_nov_neg"] / max(1e-4, ret_help["precision_nov_neg"] + ret_help["recall_nov_neg"]),
         "f1_valid_novel": 2 * ret_help["precision_valid_novel"] * ret_help["recall_valid_novel"] / max(1e-4, ret_help["precision_valid_novel"] + ret_help["recall_valid_novel"]),
         "f1_valid_nonnovel": 2 * ret_help["precision_valid_nonnovel"] * ret_help["recall_valid_nonnovel"] / max(1e-4, ret_help["precision_valid_nonnovel"] + ret_help["recall_valid_nonnovel"]),
         "f1_nonvalid_novel": 2 * ret_help["precision_nonvalid_novel"] * ret_help["recall_nonvalid_novel"] / max(1e-4, ret_help["precision_nonvalid_novel"] + ret_help["recall_nonvalid_novel"]),
@@ -98,6 +118,8 @@ def val_nov_metric(is_validity: numpy.ndarray, should_validity: numpy.ndarray, i
     })
 
     ret.update({
+        "f1_val_macro": (ret["f1_validity"] + ret["f1_val_neg"])/2,
+        "f1_nov_macro": (ret["f1_novelty"] + ret["f1_nov_neg"])/2,
         "f1_macro": (ret["f1_valid_novel"]+ret["f1_valid_nonnovel"]+ret["f1_nonvalid_novel"]+ret["f1_nonvalid_nonnovel"])/4
     })
 
